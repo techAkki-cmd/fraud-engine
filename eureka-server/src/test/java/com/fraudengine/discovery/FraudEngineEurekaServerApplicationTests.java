ff268be740a50d6c2bef81aae378fb07fa1d5a99
Arijit Ajay Kumar <2023kucp1071@iiitkota.ac.in>
2026-06-15 10:13:00 +0530

    initialize eureka discovery server and base kafka/postgres infrastructure

---
+++ eureka-server/src/test/java/com/fraudengine/discovery/FraudEngineEurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fraudengine.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FraudEngineEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
